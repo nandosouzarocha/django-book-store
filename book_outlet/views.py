54f789f801036669ada301674542991346e528a9
Nando Rocha <nando.souza.rocha@gmail.com>
2024-08-19 11:37:27 +0100

    I worked on the index and book_detail pages. The data about the books are being retrieved from the database successfully.

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Book
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "book_outlet/index.html")
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html",{
+        'books': books,
+    })
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/book_detail.html",{
+        "book": book,
+    })
